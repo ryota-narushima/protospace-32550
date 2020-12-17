b6849e169921d21abdeaec8bd603ae9ff2f29297
ryota-narushima <r.narushima1031@gmail.com>
2020-12-17 11:28:27 +0900

    authenticate_user!の対象に新投稿ページを追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: :edit
+  before_action :authenticate_user!, only: [:edit, :new]
   before_action :move_to_index, except: [:index, :show]
 
   def index
